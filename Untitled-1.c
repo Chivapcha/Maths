bfcf0f8d3e401edf17610658902b537477dda162
tedita <manon.fromont59@Gmail.com>
2025-03-07 15:51:33 +0100

    début de la hess

---
+++ Untitled-1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main(){
+    int choix;
+    printf("+--------------------------------+\n");
+    printf("|1 : Addition                    |\n");
+    printf("|2 : Soustraction                |\n");
+    printf("|3 : Multiplication              |\n");
+    printf("|4 : Tables des multiplications  |\n");
+    printf("|5 : Division                    |\n");
+    printf("|0 : Sortir du jeu               |\n");
+    printf("+--------------------------------+\n");
+    printf("Quel est votre choix ?\n");
+    scanf("%d", &choix);
+    while (choix < 0 || choix > 5) {
+        printf("Choix invalide, veuillez choisir un nombre entre 0 et 5\n");
+        scanf("%d", &choix);
+    }
+    switch(choix){
+        case 1:
+            printf("Addition\n");
+            break;
+        case 2:
+            printf("Soustraction\n");
+            break;
+        case 3:
+            printf("Multiplication\n");
+            break;
+        case 4:
+            printf("Tables de multiplication\n");
+            break;
+        case 5:
+            printf("Division\n");
+            break;
+        case 0:
+            printf("Sortie\n");
+            break;
+    }
+
+    return choix;
+}
