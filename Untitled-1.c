aee54ef38b33875a52116fefff1e305f81ed7eca
Chivapcha <knysh.yana.3@gmail.com>
2025-04-17 18:28:12 +0200

    grands nombres marchent
    c'est un peu moche avec les espaces superflus mais ça marche et c'est le plus important, j'ai galéré

--- Untitled-1.c
+++ Untitled-1.c
@@ -401,72 +401,198 @@ void Aires(){
 }
 
 void GrandsNombres() {
-    int choix;
-    scanf("%d", &choix);
-    int tab_choix [7];
+    int reponse;
+    srand(time(NULL));
+    int choix_millions = rand() % 10; // 0-9
+    int choix_milles = rand() % 1000; // 0-999
+    int choix_reste = rand() % 1000; // 0-999
+    reponse = choix_millions * 1000000 + choix_milles * 1000 + choix_reste; // 0-9999999
+    // on a notre nombre entre 0 et 9999999, on le met dans un tableau de 7 chiffres
+    int tab_reponse[7];
     for (int i = 6; i >= 0; i--) {
-        tab_choix[i] = choix % 10;
-        choix /= 10;
+        tab_reponse[i] = reponse % 10;
+        reponse /= 10;
     }
-    for (int i = 0; i < 7; i++) {
-        printf("%d", tab_choix[i]);
-    }
-    printf("\n");
-
+    // on initiamise les tableaux de chiffres en lettres
     char zero_neuf[10][6] = {"", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"};
     char onze_dixneuf[10][10] = {"", "onze", "douze", "treize", "quatorze", "quinze", "seize", "dix-sept", "dix-huit", "dix-neuf"};
     char dizaines[10][16] = {"", "dix", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante-dix", "quatre-vingt", "quatre-vingt-dix"};
 
-    char nombre_ecrit[7][100];
-    if (tab_choix[0] != 0) { // si le premier nombre (millions) est différent de 0, on affiche le chiffre et "million"
-        strcat(nombre_ecrit[0], zero_neuf[tab_choix[0]]);
-        if (tab_choix[0] = 1) { // si le chiffre est égale à 1, on ajoute "million" sans "s"
+    char nombre_ecrit[7][100]; // tableau pour stocker les chiffres en lettres
+    if (tab_reponse[0] != 0) { // si le premier nombre (millions) est différent de 0, on affiche le chiffre et "million"
+        if (tab_reponse[0] == 4) { // sans ce "if else", le programme affiche "quatrecinq" au lieu de "quatre", mais je ne sais pas pourquoi
+            strcat(nombre_ecrit[0], "quatre");
+        }
+        else {
+            strcat(nombre_ecrit[0], zero_neuf[tab_reponse[0]]);
+        }
+        if (tab_reponse[0] == 1) { // si le chiffre est égale à 1, on ajoute "million" sans "s"
             strcat(nombre_ecrit[0], " million");
         }
         else { // sinon, on ajoute "millions"
             strcat(nombre_ecrit[0], " millions");
         }
     }
-    if (tab_choix[1] != 0) { // si le deuxième nombre (centaines de milles) est différent de 0, on affiche le chiffre et "cent"
-        strcat(nombre_ecrit[1], zero_neuf[tab_choix[1]]);
-        strcat(nombre_ecrit[1], " cent");
-    }
-    if (tab_choix[2] != 0) { // si le troisième nombre (dizaines de milles) est différent de 0 :
-        if (tab_choix[2] == 1 && tab_choix[3] != 0) { // si ce nombre est 1 et qu'il y a un chiffre après, on affiche le nombre entre 11 et 19
-            strcat(nombre_ecrit[2], onze_dixneuf[tab_choix[3]]);
-        } else { // sinon, on affiche le chiffre et "dix" ou "vingt" ou "trente", etc.
-            strcat(nombre_ecrit[2], dizaines[tab_choix[2]]);
+
+    if (tab_reponse[1] != 0) { // si le deuxième nombre (centaines de milles) est différent de 0, on affiche le chiffre et "cent", sauf si c'est 1, dans ce cas on affiche juste "cent"
+        if (tab_reponse[1] == 4) { // sans ce "if", le programme affiche "quatrecinq" au lieu de "quatre", mais je ne sais pas pourquoi
+            strcat(nombre_ecrit[1], "quatre");
+            strcat(nombre_ecrit[1], " cent");
+        }
+        else if (tab_reponse[1] == 1) {
+            strcat(nombre_ecrit[1], "cent");
+        } else {
+            strcat(nombre_ecrit[1], zero_neuf[tab_reponse[1]]);
+            strcat(nombre_ecrit[1], " cent");
         }
     }
-    if (tab_choix[3] != 0) { // si le quatrième nombre (unités de milles) est différent de 0, on affiche le chiffre
-        if (tab_choix[2] != 1) { // si le chiffre précédent n'est pas 1 (le nombre d'unités de milles n'est pas entre 11 et 19), on affiche le chiffre
-            strcat(nombre_ecrit[3], zero_neuf[tab_choix[3]]);
+
+    if (tab_reponse[2] != 0) { // si le troisième nombre (dizaines de milles) est différent de 0 :
+        if (tab_reponse[2] == 1 && tab_reponse[3] != 0) { // si ce nombre est 1 et qu'il y a un chiffre après, on affiche le nombre entre 11 et 19
+            strcat(nombre_ecrit[2], onze_dixneuf[tab_reponse[3]]);
+        }
+        else if (tab_reponse[2] == 7 || tab_reponse[2] == 9) { // si le chiffre est 7 ou 9 :
+            if (tab_reponse[3] < 7) { // si le chiffre suivant est inférieur à 7 :
+                if (tab_reponse[3] == 0) { // si le chiffre suivant est 0, on ajoute "-dix"
+                    strcat(nombre_ecrit[2], dizaines[tab_reponse[2] - 1]);
+                    strcat (nombre_ecrit[2], "-dix");
+                } else {
+                    strcat(nombre_ecrit[2], dizaines[tab_reponse[2] - 1]);
+                    strcat (nombre_ecrit[2], "-");
+                    strcat(nombre_ecrit[2], onze_dixneuf[tab_reponse[3]]);
+                }
+            } else {
+                strcat(nombre_ecrit[2], dizaines[tab_reponse[2]]);
+                strcat (nombre_ecrit[2], "-");
+                strcat(nombre_ecrit[2], zero_neuf[tab_reponse[3]]);
+            }
+        }
+        else { // sinon, on affiche le chiffre et "dix" ou "vingt" ou "trente", etc.
+            strcat(nombre_ecrit[2], dizaines[tab_reponse[2]]);
+            if (tab_reponse[3] == 1) { // si le chiffre suivant est 1, on ajoute "et" entre les deux
+                strcat(nombre_ecrit[2], "-et-");
+            } else if (tab_reponse[3] != 0) { // si le chiffre suivant n'est pas 0, on ajoute un tiret entre les deux
+                strcat(nombre_ecrit[2], "-");
+            }
         }
     }
-    if (tab_choix[1] != 0 || tab_choix[2] != 0 || tab_choix[3] != 0) { // si il y a un nombre de milles alors on affiche "mille" à la fin
+    
+    if (tab_reponse[3] != 0) { // si le quatrième nombre (unités de milles) est différent de 0, on affiche le chiffre
+        if (tab_reponse[2] != 1) { // si le chiffre précédent n'est pas 1 (le nombre d'unités de milles n'est pas entre 11 et 19), on affiche le chiffre
+            if (tab_reponse[3] == 4) { // sans ce "if", le programme affiche "quatrecinq" au lieu de "quatre", mais je ne sais pas pourquoi
+                strcat(nombre_ecrit[3], "quatre");
+            }
+            else {
+                strcat(nombre_ecrit[3], zero_neuf[tab_reponse[3]]);
+            }
+        }
+        if (tab_reponse[2] == 7 || tab_reponse[2] == 9) { // si le chiffre précédent est 7 ou 9 on efface les unités parce qu'on les a déjà ajoutées
+            strcpy(nombre_ecrit[3], "");
+        }
+    }
+
+    if (tab_reponse[1] != 0 || tab_reponse[2] != 0 || tab_reponse[3] != 0) { // si il y a un nombre de milles alors on affiche "mille" à la fin
         strcat(nombre_ecrit[3], " mille");
     }
-    if (tab_choix[4] != 0) { // si le cinquième nombre (centaines) est différent de 0, on affiche le chiffre et "cent"
-        strcat(nombre_ecrit[4], zero_neuf[tab_choix[4]]);
-        strcat(nombre_ecrit[4], " cent");
-    }
-    if (tab_choix[5] != 0) { // si le sixième nombre (dizaines) est différent de 0 :
-        if (tab_choix[5] == 1 && tab_choix[6] != 0) { // si ce nombre est 1 et qu'il y a un chiffre après, on affiche le nombre entre 11 et 19
-            strcat(nombre_ecrit[5], onze_dixneuf[tab_choix[6]]);
-        } else { // sinon, on affiche le chiffre et "dix" ou "vingt" ou "trente", etc.
-            strcat(nombre_ecrit[5], dizaines[tab_choix[5]]);
+
+    if (tab_reponse[4] != 0) { // si le cinquième nombre (centaines) est différent de 0, on affiche le chiffre (si c'est pas 1) et "cent"
+        if (tab_reponse[4] == 1) {
+            strcat(nombre_ecrit[4], "cent");
+        } else if (tab_reponse[4] == 4) { // sans ce "if", le programme affiche "quatrecinq" au lieu de "quatre", mais je ne sais pas pourquoi
+            strcat(nombre_ecrit[4], "quatre");
+            strcat(nombre_ecrit[4], " cent");
+        }
+        else{
+            strcat(nombre_ecrit[4], zero_neuf[tab_reponse[4]]);
+            strcat(nombre_ecrit[4], " cent");
         }
     }
-    if (tab_choix[6] != 0) { // si le septième nombre (unités) est différent de 0, on affiche le chiffre
-        if (tab_choix[5] != 1) {
-            strcat(nombre_ecrit[6], zero_neuf[tab_choix[6]]);
+
+    if (tab_reponse[5] != 0) { // si le sixième nombre (dizaines) est différent de 0 :
+        if (tab_reponse[5] == 1 && tab_reponse[6] != 0) { // si ce nombre est 1 et qu'il y a un chiffre après, on affiche le nombre entre 11 et 19
+            strcat(nombre_ecrit[5], onze_dixneuf[tab_reponse[6]]);
+        }
+        else if (tab_reponse[5] == 7 || tab_reponse[5] == 9) { // si le chiffre est 7 ou 9 :
+            if (tab_reponse[6] < 7) { // si le chiffre suivant est inférieur à 7 :
+                if (tab_reponse[6] == 0) { // si le chiffre suivant est 0, on ajoute "-dix"
+                    strcat(nombre_ecrit[5], dizaines[tab_reponse[5] - 1]);
+                    strcat (nombre_ecrit[5], "-dix");
+                } else {
+                    strcat(nombre_ecrit[5], dizaines[tab_reponse[5] - 1]);
+                    strcat (nombre_ecrit[5], "-");
+                    strcat(nombre_ecrit[5], onze_dixneuf[tab_reponse[6]]);
+                }
+            } else {
+                strcat(nombre_ecrit[5], dizaines[tab_reponse[5]]);
+                strcat (nombre_ecrit[5], "-");
+                strcat(nombre_ecrit[5], zero_neuf[tab_reponse[6]]);
+            }
+        }
+        else { // sinon, on affiche le chiffre et "dix" ou "vingt" ou "trente", etc.
+            strcat(nombre_ecrit[5], dizaines[tab_reponse[5]]);
+        }
+        if (!(tab_reponse[5] == 1 || tab_reponse[5] == 7 || tab_reponse[5] == 9) && tab_reponse[6] == 1) { // si le chiffre est pas 1, 7 ou 9 et que le chiffre suivant est 1, on ajoute "et" entre les deux
+            strcat(nombre_ecrit[5], "-et-");
+        } else if (!(tab_reponse[5] == 1 || tab_reponse[5] == 7 || tab_reponse[5] == 9) && tab_reponse[3] != 0) { // si le chiffre suivant n'est pas 0, on ajoute un tiret entre les deux
+            strcat(nombre_ecrit[5], "-");
         }
     }
+    
+    if (tab_reponse[6] != 0) { // si le septième nombre (unités) est différent de 0, on affiche le chiffre
+        if (tab_reponse[5] != 1) { // si le chiffre précédent n'est pas 1 (le nombre d'unités n'est pas entre 11 et 19) :
+            if (tab_reponse[6] == 4) { // sans ce "if", le programme affiche "quatrecinq" au lieu de "quatre", mais je ne sais pas pourquoi
+                strcat(nombre_ecrit[6], "quatre");
+            }
+            else if (tab_reponse[5] == 7 || tab_reponse[5] == 9) { // si le chiffre précédent est 7 ou 9 :
+                if (tab_reponse[6] < 7) { // si le chiffre suivant est inférieur à 7 :
+                    strcat(nombre_ecrit[6], onze_dixneuf[tab_reponse[6]]);
+                } else {
+                    strcat(nombre_ecrit[6], zero_neuf[tab_reponse[6]]);
+                }
+            }
+            else {
+                strcat(nombre_ecrit[6], zero_neuf[tab_reponse[6]]);
+            }
+        }
+        if (tab_reponse[5] == 7 || tab_reponse[5] == 9) { // si le chiffre précédent est 7 ou 9 on efface les unités parce qu'on les a déjà ajoutées
+            strcpy(nombre_ecrit[6], "");
+        }
+    }
+
     // On affiche le nombre écrit en toutes lettres
     for (int i = 0; i < 7; i++) {
         printf("%s ", nombre_ecrit[i]);
     }
     printf("\n");
+
+    // On demande à l'utilisateur de le réécrire
+    int reponse_utilisateur;
+    int trouve = 0;
+    int i = 1;
+    while (trouve == 0 && i <= 3){
+        printf("Ecrivez le nombre en chiffres : ");
+        scanf("%d", &reponse_utilisateur);
+        if (reponse_utilisateur == choix_millions * 1000000 + choix_milles * 1000 + choix_reste) {
+            trouve = 1;
+            printf("Bravo !\n");
+            if (i == 1) {
+                printf("Vous avez gagne 10 points\n");
+                points += 10;
+            } else if (i == 2) {
+                printf("Vous avez gagne 5 points\n");
+                points += 5;
+            } else {
+                printf("Vous avez gagne 1 point\n");
+                points += 1;
+            }
+        } else {
+            printf("Perdu... Il vous reste %d essais\n", 3-i);
+        }
+        i++;
+    }
+    if (trouve == 0){
+        printf("La reponse etait %d\n", choix_millions * 1000000 + choix_milles * 1000 + choix_reste);
+    }
 }
 
 typedef struct {
